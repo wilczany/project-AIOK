840e0b7ad55196803d526e90eb19da1b89e5ed19
papryku <supteros@gmail.com>
2024-01-14 03:50:53 +0100

    realizacja wymagań
    dodatkowe informacje zwiazane z zasobami do wymagań oraz ich aktualną realizacją

--- src/LessonCell.tsx
+++ src/LessonCell.tsx
@@ -8,6 +8,7 @@ interface Props {
 
 // komponent odpowiedzialny za wypełnienie komórki w tabeli danymi o lekcji
 // używalny w tabeli lekcji klasy, nauczyciela oraz sali, bo przyjmuje jedynie lekcję do sformatowania oraz wypisania w krótkim formacie
+// komponent prezentacyjny 1/2; komponent reużywalny 1/4
 function LessonCell({lesson}: Props): React.ReactNode{
     return (
         <span>{lesson.Nazwa} {lesson.Sala.Id} {lesson.Nauczyciel.ShortName}</span>
